d80cb219e2d167e106be7d23316e1023179e258b
devlikebear <devlikebear@gmail.com>
2026-05-25 20:07:17 +0900

    feat(engine): wire node + snapshot repos and handlers

--- engine/cmd/linetta-engine/main.go
+++ engine/cmd/linetta-engine/main.go
@@ -13,10 +13,12 @@ import (
 
 	_ "github.com/devlikebear/tars/pkg/llm" // pin
 
+	"github.com/devlikebear/linetta/engine/internal/node"
 	"github.com/devlikebear/linetta/engine/internal/paths"
 	"github.com/devlikebear/linetta/engine/internal/project"
 	"github.com/devlikebear/linetta/engine/internal/rpc"
 	"github.com/devlikebear/linetta/engine/internal/rpc/handlers"
+	"github.com/devlikebear/linetta/engine/internal/snapshot"
 	"github.com/devlikebear/linetta/engine/internal/store"
 )
 
@@ -46,15 +48,21 @@ func main() {
 	}
 	defer st.Close()
 
-	repo := project.NewRepo(st)
+	projects := project.NewRepo(st)
+	nodes := node.NewRepo(st)
+	snaps := snapshot.NewRepo(st)
 	clock := func() int64 { return time.Now().UnixMilli() }
 
 	s := rpc.NewServer()
 	s.Handle("ping", handlers.Ping)
-	s.Handle("projects.create", handlers.CreateProject(repo, clock))
-	s.Handle("projects.list", handlers.ListProjects(repo))
-	s.Handle("projects.get", handlers.GetProject(repo))
-	s.Handle("projects.archive", handlers.ArchiveProject(repo, clock))
+	s.Handle("projects.create", handlers.CreateProject(projects, clock))
+	s.Handle("projects.list", handlers.ListProjects(projects))
+	s.Handle("projects.get", handlers.GetProject(projects))
+	s.Handle("projects.archive", handlers.ArchiveProject(projects, clock))
+	s.Handle("nodes.get", handlers.GetNode(nodes))
+	s.Handle("nodes.update_content", handlers.UpdateNodeContent(nodes, snaps, clock))
+	s.Handle("nodes.set_last_opened", handlers.SetLastOpened(nodes, clock))
+	s.Handle("snapshots.create_manual", handlers.CreateManualSnapshot(snaps, clock))
 
 	if err := s.Serve(ctx, os.Stdin, os.Stdout); err != nil && !errors.Is(err, io.EOF) {
 		fail("serve: %v", err)
