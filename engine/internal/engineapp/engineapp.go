b95f979b96d280b11a88bdc37c53ead2a00b64b4
devlikebear <devlikebear@gmail.com>
2026-06-21 18:23:44 +0900

    release: v0.8.2

--- engine/internal/engineapp/engineapp.go
+++ engine/internal/engineapp/engineapp.go
@@ -40,7 +40,7 @@ import (
 	"github.com/devlikebear/linetta/engine/internal/thread"
 )
 
-const DefaultVersion = "0.8.1"
+const DefaultVersion = "0.8.2"
 
 // Options configures an embedded engine instance.
 type Options struct {
